02a793cda0e24dea4fcaf087dba2920200fd1ac3
Timothy J. Baek <timothyjrbeck@gmail.com>
2024-07-16 14:40:19 +0200

    rename

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-docker build -t on-discord .
-docker rm -f on-discord || true
+docker build -t dog3 .
+docker rm -f dog3 || true
 
-docker run -d --add-host=host.docker.internal:host-gateway -e DISCORD_TOKEN='' -e DB_URL='mongodb://host.docker.internal:27017/?authSource=admin' --name on-discord --restart always on-discord
+docker run -d --add-host=host.docker.internal:host-gateway -e DISCORD_TOKEN='' -e DB_URL='mongodb://host.docker.internal:27017/?authSource=admin' --name dog3 --restart always dog3
 docker image prune -f
--- db/index.js
+++ db/index.js
@@ -2,7 +2,7 @@ const { MongoClient } = require("mongodb");
 
 const uri = `${process.env.DB_URL}`;
 const mongoClient = new MongoClient(uri);
-const database = mongoClient.db("on-discord");
+const database = mongoClient.db("dog3");
 
 const insertMessage = async (item) => {
   const Messages = database.collection("messages");
